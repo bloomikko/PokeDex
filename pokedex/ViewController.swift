6cb0fddf88c3995aca972d26a8b469cb5e51ae9f
Mikko Rouru <mikko.rouru@gmail.com>
2017-11-30 14:04:40 +0200

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Mikko Rouru on 30.11.2017.
+//  Copyright © 2017 Mikko Rouru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
